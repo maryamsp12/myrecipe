e58cc35328634f75ea38c7eb822a06792aded729
maryamsp12 <maryamsp12@example.com>
2019-01-29 19:43:18 +0200

    Updates

---
+++ precache-manifest.d41cf6f351dacb7eee6372a7ac004a12.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "693457eb8e1e72c33441",
+    "url": "/recipe-finder/static/js/runtime~main.693457eb.js"
+  },
+  {
+    "revision": "0d41c06ed56633ece890",
+    "url": "/recipe-finder/static/js/main.0d41c06e.chunk.js"
+  },
+  {
+    "revision": "49ea011ab5bbe92a42f9",
+    "url": "/recipe-finder/static/js/1.49ea011a.chunk.js"
+  },
+  {
+    "revision": "0d41c06ed56633ece890",
+    "url": "/recipe-finder/static/css/main.d1dcaf16.chunk.css"
+  },
+  {
+    "revision": "49ea011ab5bbe92a42f9",
+    "url": "/recipe-finder/static/css/1.6f78b4c2.chunk.css"
+  },
+  {
+    "revision": "e229770e7c869be2914ccc7904a60f2b",
+    "url": "/recipe-finder/index.html"
+  }
+];
